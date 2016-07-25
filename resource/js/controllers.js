7cbc93fe2f70e07ad205e1032e11485740a0e921
amontoya49 <andresmhkun@gmail.com>
2016-07-24 21:20:14 -0500

    final
    end of tutorial!

--- resource/js/controllers.js
+++ resource/js/controllers.js
@@ -1,4 +1,35 @@
 angular.module('FinalApp')
-.controller('MainController', function($scope){
-	
+.controller('MainController', function($scope, $resource, PostResource){
+	User = $resource("https://jsonplaceholder.typicode.com/users/:id", {id: "@id"});
+	$scope.posts = PostResource.query();
+	$scope.users = User.query();
+	$scope.removePost = function(post){
+		PostResource.delete({id: post.id}, function(data){
+			console.log(data);
+			// se volveria a cargar el arreglo si la API fuera nuestra
+			// $scope.post = Post.query();
+		});
+		$scope.posts = $scope.posts.filter(function(element){
+			return element.id !== post.id;
+		});
+	}
+
+})
+.controller('PostController', function($scope,PostResource, $routeParams){
+	$scope.title = "Edit Post";
+	$scope.post = PostResource.get({id: $routeParams.id});
+	$scope.savePost = function(){
+		PostResource.update({id: $scope.post.id},{data: $scope.post}, function(data){
+			console.log(data);
+		});
+	}
+})
+.controller('NewPostController', function($scope, PostResource){
+	$scope.post = {};
+	$scope.title = "Create post"
+	$scope.savePost = function(){
+		PostResource.save({data: $scope.post}, function(data){
+			console.log(data);
+		});
+	}
 });
